3d6e4e30c04424c198385ba304b9bffaa9e980cb
Omaru Jawara <omarito.jawara@gmail.com>
2022-07-12 23:15:24 -0400

    sys module typo fix

--- scripts/activate.py
+++ scripts/activate.py
@@ -1,12 +1,12 @@
 import os
-from system import platform
+from sys import platform
 
 #windows
 if platform == "win32":
-    os.system("venv/bin/activate")
+    os.system("../venv/bin/activate")
 
  #mac or linux
 if platform == "darwin" or platform == "linux" or platform == "linux2":
-    os.system("venv\Scripts\activate.bat")
+    os.system("../venv/Scripts/activate.bat")
 
 os.system("pip install -r requirements.txt")
