5e9d93e7bbe8600b2ac0a8e7d50c308cfd7e7410
Omaru Jawara <omarito.jawara@gmail.com>
2022-07-12 23:57:27 -0400

    system command fix

--- scripts/activate.py
+++ scripts/activate.py
@@ -3,12 +3,12 @@ from sys import platform
 
 #windows
 if platform == "win32":
-    os.system("../venv/bin/activate")
+    os.system("cd ..")
+    os.system("venv\Scripts\activate.bat")
 
  #mac or linux
 if platform == "darwin" or platform == "linux" or platform == "linux2":
-    os.system("cd ..")
-    os.system("/venv/Scripts/activate.bat")
+    os.system("source ../venv/bin/activate")
 
 
 os.system("pip install -r requirements.txt")
